98f9177ad4a3426a8df53d43b81434a6fcc1d241
tevos matevosyan <97086468+tevops@users.noreply.github.com>
2023-03-15 10:36:31 +0400

    Update test_env.py

--- tests/integraion/test_env.py
+++ tests/integraion/test_env.py
@@ -5,12 +5,3 @@ from debugger.pipeline_runner.runner import get_analytics_pem
 def test_aws_setup():
     assert Path('~/.aws/config').exists()
     assert Path('~/.aws/credentials').exists()
-
-
-def test_agmri_setup():
-    assert Path('~/.agmri.cfg').exists()
-
-
-def test_os_env():
-    pem_path = get_analytics_pem()
-    assert Path(pem_path).exists()
